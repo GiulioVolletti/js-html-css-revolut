e6c9927511e24d7ed51d6a75802c5c3607cb7a14
Giulio <giuliovolletti@hotmail.com>
2020-11-25 17:03:55 +0100

    commit 5

--- js/script.js
+++ js/script.js
@@ -17,5 +17,20 @@ $( document ).ready(
            $("a").removeClass("this-active")
       }
     )
+
+    $(".bonus").mouseenter(
+      function(){
+        console.log($(this).children().hasClass("bonus-container"));
+        if ($(this).children().hasClass("bonus-container") == true) {
+          $(this).children().toggleClass("this-active")
+        }
+      }
+    )
+      $(".bonus-container").mouseleave(
+        function(){
+           $(".bonus-container").removeClass("this-active")
+           $("a").removeClass("this-active")
+      }
+    )
   }
 )
