24120d9a0bbfa08870637d9e5087aa2c6a53a619
Giulio <giuliovolletti@hotmail.com>
2020-11-25 16:37:24 +0100

    commit 4

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $( document ).ready(
       $(".dropdown").mouseleave(
         function(){
            $(".dropdown").removeClass("this-active")
+           $("a").removeClass("this-active")
       }
     )
   }
