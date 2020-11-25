a5d88bf0809f7e0d3dd383d62a39c34f4246ecf8
Giulio <giuliovolletti@hotmail.com>
2020-11-25 18:06:19 +0100

    commit 6

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@
 $( document ).ready(
   function (){
     console.log("hello world");
+
     $("li").mouseenter(
       function(){
-        console.log($(this).children().hasClass("dropdown"));
+        // console.log($(this).children().hasClass("dropdown"));
         if ($(this).children().hasClass("dropdown") == true) {
           $(this).children().toggleClass("this-active")
         }
@@ -17,20 +18,40 @@ $( document ).ready(
            $("a").removeClass("this-active")
       }
     )
-
+    // -----------------------------click----------------------- //
+    $("li").click(
+      function(){
+        // console.log($(this).children().hasClass("dropdown"));
+        if ($(this).children().hasClass("dropdown") == true) {
+          $(this).children().toggleClass("this-active")
+        }
+      }
+    )
+    // ----------------------------bonus------------------------------- //
     $(".bonus").mouseenter(
       function(){
-        console.log($(this).children().hasClass("bonus-container"));
+        // console.log($(this).children().hasClass("bonus-container"));
         if ($(this).children().hasClass("bonus-container") == true) {
           $(this).children().toggleClass("this-active")
         }
       }
     )
+
       $(".bonus-container").mouseleave(
         function(){
            $(".bonus-container").removeClass("this-active")
            $("a").removeClass("this-active")
       }
     )
+    // -----------------------------bonus click----------------------- //
+    $(".bonus").click(
+      function(){
+
+        if ($(this).children().hasClass("bonus-container") == true) {
+          $(this).children().toggleClass("this-active")
+        }
+      }
+    )
+
   }
 )
